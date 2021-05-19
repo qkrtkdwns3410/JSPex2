9d079c084be297f2ade4817851ad1300b343d1bf
psj <qkrtkdwns3410>
2021-05-19 19:14:44 +0900

    5-19

--- src/com/newlecture/web/NoticeReg.java
+++ src/com/newlecture/web/NoticeReg.java
@@ -14,7 +14,8 @@ public class NoticeReg extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     
         response.setContentType("text/html; charset=UTF-8");
-    
+        request.setCharacterEncoding("UTF-8");
+        
         PrintWriter out = response.getWriter();
     
         String title = request.getParameter("title");
